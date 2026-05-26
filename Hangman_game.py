f2f4dccd73bccffe70fbf8bad4c317ef0fe079c4
jayrohit0310 <jayrohit0310@gmail.com>
2026-05-26 12:26:25 +0530

    Add files via upload

---
+++ Hangman_game.py
@@ -0,0 +1,42 @@
+import random
+
+words = ["apple", "tiger", "teacher", "robot", "science"]
+word = random.choice(words)
+
+guessed_letters = []
+lives = 6
+
+print("--------- Welcome to Hangman Game! ---------")
+
+while lives > 0:
+    display_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("\nWord:", display_word)
+
+    if "_" not in display_word:
+        print("Congratulations! You guessed the word:", word)
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        print("Correct Guess!")
+    else:
+        lives -= 1
+        print("Wrong Guess!")
+        print("Lives remaining:", lives)
+
+if lives == 0:
+    print("\nGame Over!")
+    print("The word was:", word)
+    
